5dd770e337595005faac00b3596d9ae342fcd0b7
Joella Flood <joella.flood@mycit.ie>
2018-02-23 12:14:46 +0000

    Initial commit

---
+++ src/main/java/ie/cit/soft8027/helloworld/controller/HomeController.java
@@ -0,0 +1,18 @@
+package ie.cit.soft8027.helloworld.controller;
+
+import java.util.Locale;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+	@GetMapping(value = "/")
+	public String home(Locale locale, Model model) {
+
+		return "home";
+
+	}
+}
